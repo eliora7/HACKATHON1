bb199c34f28722364dd6659f931d77927bfd8cad
jbt <elioral770@gmail.com>
2024-09-18 13:53:13 +0300

    MAIN AND CONSTS

---
+++ CONSTS.py
@@ -0,0 +1,60 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Pictures of actions:
+FOOD = pygame.image.load("food.png")
+FOOD = pygame.transform.scale(FOOD, (250, 250), )
+CLEAN = pygame.image.load("clean.png")
+CLEAN = pygame.transform.scale(CLEAN, (250, 250), )
+FIX = pygame.image.load("fix.png")
+FIX = pygame.transform.scale(FIX, (250, 250), )
+STUFF = pygame.image.load("stuff.png")
+STUFF = pygame.transform.scale(STUFF, (250, 250), )
+TALKING = pygame.image.load("talking.png")
+TALKING = pygame.transform.scale(TALKING, (250, 250), )
+LAWYER = pygame.image.load("lawyer.png")
+LAWYER = pygame.transform.scale(LAWYER, (250, 250), )
+LOGO = pygame.image.load("logo.png")
+LOGO = pygame.transform.scale(LOGO, (670, 700), )
+SMALL_LOGO = pygame.image.load("logo.png")
+SMALL_LOGO = pygame.transform.scale(SMALL_LOGO, (150, 150), )
+TITLE = pygame.image.load("title.png")
+TITLE = pygame.transform.scale(TITLE, (700, 200), )
+BYE = pygame.image.load("bye.png")
+BYE = pygame.transform.scale(BYE, (500, 600), )
+
+# Photos of the volunteers:
+PEOPLE1 = pygame.image.load("people1.png")
+PEOPLE1 = pygame.transform.scale(PEOPLE1, (670, 700), )
+PEOPLE2 = pygame.image.load("people2.png")
+PEOPLE2 = pygame.transform.scale(PEOPLE2, (670, 700), )
+PEOPLE3 = pygame.image.load("people3.png")
+PEOPLE3 = pygame.transform.scale(PEOPLE3, (670, 700), )
+PEOPLE4 = pygame.image.load("people4.png")
+PEOPLE4 = pygame.transform.scale(PEOPLE4, (670, 700), )
+PEOPLE5 = pygame.image.load("people5.png")
+PEOPLE5 = pygame.transform.scale(PEOPLE5, (670, 700), )
+PEOPLE6 = pygame.image.load("people6.png")
+PEOPLE6 = pygame.transform.scale(PEOPLE6, (670, 700), )
+
+# Photos of the elderly people:
+ELDER1 = pygame.image.load("elder1.png")
+ELDER1 = pygame.transform.scale(ELDER1, (670, 700), )
+ELDER2 = pygame.image.load("elder2.png")
+ELDER2 = pygame.transform.scale(ELDER2, (670, 700), )
+ELDER3 = pygame.image.load("elder3.png")
+ELDER3 = pygame.transform.scale(ELDER3, (670, 700), )
+ELDER4 = pygame.image.load("elder4.png")
+ELDER4 = pygame.transform.scale(ELDER4, (670, 700), )
+ELDER5 = pygame.image.load("elder5.png")
+ELDER5 = pygame.transform.scale(ELDER5, (670, 700), )
+ELDER6 = pygame.image.load("elder6.png")
+ELDER6 = pygame.transform.scale(ELDER6, (670, 700), )
+
+ELDERLY_PEOPLE_LIST = [["0509887622", ELDER1], ["0544478914", ELDER2], ["0529883455", ELDER3],
+                       ["0501665897", ELDER4], ["0522115633", ELDER5], ["0525593288", ELDER6]]
+ELDERLY_PHONE_NUM = ["0509887622", "0544478914", "0529883455", "0501665897", "0522115633", "0525593288"]
+ELDERLY_PEOPLE_DICT = {"ELDER1": "0509887622", "ELDER2": "0544478914", "ELDER3": "0529883455", "ELDER4": "0501665897",
+                       "ELDER5": "0522115633", "ELDER6": "0525593288"}
---
+++ MAIN.py
@@ -0,0 +1,160 @@
+from CONSTS import *
+
+pygame.init()
+screen = pygame.display.set_mode((1500, 1000))
+pygame.display.set_caption("Golden Support")
+# Creating the screen:
+screen3 = pygame.display.set_mode((1500, 1000))
+screen3.fill((240, 194, 70))
+screen_display = pygame.display
+screen3.blit(LOGO, (400, 100))
+pygame.display.flip()
+phone_num_elder = input("Please enter your phone number to enter the application: ")
+if phone_num_elder not in ELDERLY_PHONE_NUM:
+    print("We are sorry you are not registered for the application :( ")
+    print("contact us to register: 0504667841- Golden Support")
+else:
+    if phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER1"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER1, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+
+    elif phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER2"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER2, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+
+    elif phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER3"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER3, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+
+    elif phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER4"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER4, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+
+    elif phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER5"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER5, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+
+    elif phone_num_elder == ELDERLY_PEOPLE_DICT["ELDER6"]:
+        screen3 = pygame.display.set_mode((1500, 1000))
+        screen3.fill((240, 194, 70))
+        screen_display = pygame.display
+        screen3.blit(ELDER6, (400, 100))
+        screen.blit(SMALL_LOGO, (20, 20))
+        screen_display.update()
+        pygame.time.delay(3000)
+    pygame.display.flip()
+
+    running = True
+    while running:
+        screen.fill((240, 194, 70))
+        screen.blit(TITLE, (400, 50))
+        screen.blit(FOOD, (200, 250))
+        screen.blit(CLEAN, (600, 250))
+        screen.blit(FIX, (1050, 250))
+        screen.blit(STUFF, (200, 600))
+        screen.blit(TALKING, (600, 600))
+        screen.blit(LAWYER, (1050, 600))
+        screen.blit(SMALL_LOGO, (20, 20))
+
+        # Performing actions in the game:
+        for event in pygame.event.get():
+            # Clicking on the red X:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE1, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    running = False
+                    pygame.time.delay(3000)
+
+                elif event.key == pygame.K_2:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE2, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    running = False
+                    pygame.time.delay(3000)
+
+                elif event.key == pygame.K_3:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE3, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    running = False
+                    pygame.time.delay(3000)
+
+                elif event.key == pygame.K_4:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE4, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    running = False
+                    pygame.time.delay(3000)
+
+                elif event.key == pygame.K_5:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE5, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    running = False
+                    pygame.time.delay(3000)
+
+                elif event.key == pygame.K_6:
+                    screen2 = pygame.display.set_mode((1500, 1000))
+                    screen2.fill((255, 228, 181))
+                    screen.blit(PEOPLE6, (400, 100))
+                    screen.blit(SMALL_LOGO, (20, 20))
+                    screen_display = pygame.display
+                    screen_display.update()
+                    pygame.time.delay(3000)
+                    running = False
+            pygame.display.flip()
+
+    last_screen = pygame.display.set_mode((1500, 1000))
+    last_screen.fill((240, 194, 70))
+    screen_display = pygame.display
+    screen.blit(BYE, (550, 200))
+    screen.blit(SMALL_LOGO, (20, 20))
+    screen_display.update()
+    pygame.time.delay(3000)
+    pygame.display.flip()
